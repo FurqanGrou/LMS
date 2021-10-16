509796d5dd550201b1a247bda07ba403c6582699
Aziz <91914267+MAzizFurqan@users.noreply.github.com>
2021-10-16 19:50:35 +0200

    test staging

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    الكريم مركز الفرقان لتعليم القرآن
+                    يا مسهل
                 </div>
 
                 <div class="links">
