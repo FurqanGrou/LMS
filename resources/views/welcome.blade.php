feff7510a899cee94b02c0026529dc93a5351b52
Aziz <91914267+MAzizFurqan@users.noreply.github.com>
2021-10-16 19:53:07 +0200

    Tesr the auto deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    يا مسهل
+                    مجموعةالفرقان لتعليم القرآن الكريم
                 </div>
 
                 <div class="links">
