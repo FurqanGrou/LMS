8c07367bbc917fee2b120c0416b824678d9ecbc0
Aziz <91914267+MAzizFurqan@users.noreply.github.com>
2021-10-16 19:10:23 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    مركز الفرقان لتعليم القرآن
+                    مركز الفرقان لتعليم القرآن الكريم
                 </div>
 
                 <div class="links">
