1c1fb9c0253d30780cd519643706874a2b94dde9
Aziz <91914267+MAzizFurqan@users.noreply.github.com>
2021-10-17 11:52:36 +0200

    test auto dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    مجموعة الفرقان لتعليم القرآن الكريم
+                   بسم الله
                 </div>
 
                 <div class="links">
