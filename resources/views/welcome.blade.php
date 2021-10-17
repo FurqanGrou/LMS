ac3f1d7fa317459101aa9658ef806d21970e55c7
Aziz <91914267+MAzizFurqan@users.noreply.github.com>
2021-10-17 11:56:21 +0200

    dev test auto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   بسم الله
+                   It should work
                 </div>
 
                 <div class="links">
