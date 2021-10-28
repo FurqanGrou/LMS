4ba2ed824fd102ff7a019be6baae0725f8559e85
Hatim Hussein <hatem.fouad@furqangroup.com>
2021-10-28 13:11:43 +0300

    Css updates
    Css updates

--- resources/views/components/attendance.blade.php
+++ resources/views/components/attendance.blade.php
@@ -8,7 +8,7 @@
             <button type="submit" class="btn btn-success" name="login_btn" value="login">
                 <i class="ft-log-in"></i> دخول
             </button>
-            <p class="white mt-1">{{ \Carbon\Carbon::now()->hour . ':' . \Carbon\Carbon::now()->minute }}</p>
+            <p class="white mt-1" style="margin-left: 5px; margin-right: 5px;">{{ \Carbon\Carbon::now()->hour . ':' . \Carbon\Carbon::now()->minute }}</p>
             <button type="submit" class="btn btn-danger" name="logout_btn" value="logout">
                 <i class="ft-log-out"></i> خروج
             </button>
