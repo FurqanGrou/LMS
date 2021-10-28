0b4b7b56f4eafc50aa463e3a0b8f0ae3b5247d0f
Hatim Hussein <hatem.fouad@furqangroup.com>
2021-10-28 14:24:42 +0300

    201
    Service Request - Exam Request

--- resources/views/teachers/layouts/side-menu.blade.php
+++ resources/views/teachers/layouts/side-menu.blade.php
@@ -24,13 +24,32 @@
                 </ul>
             </li>
 
-            <li class=" nav-item"><a href="#"><i class="la la-folder"></i><span class="menu-title" data-i18n="nav.templates.main">الشكاوى والاقتراحات</span></a>
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="la la-folder"></i>
+                    <span class="menu-title" data-i18n="nav.templates.main">الشكاوى والاقتراحات</span>
+                </a>
                 <ul class="menu-content">
                     <li>
                         <a class="menu-item" href="{{ route('teachers.request_services.index') }}" data-i18n="nav.templates.vert.classic_menu">عرض الشكاوى والاقتراحات المقدمة</a>
                     </li>
                     <li>
                         <a class="menu-item" href="{{ route('teachers.request_services.create') }}" data-i18n="nav.templates.vert.classic_menu">تقديم شكوى أو مقترح</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class=" nav-item">
+                <a href="#">
+                    <i class="la la-file-text"></i>
+                    <span class="menu-title" data-i18n="nav.templates.main">طلبات الاختبارات</span>
+                </a>
+                <ul class="menu-content">
+                    <li>
+                        <a class="menu-item" href="#" data-i18n="nav.templates.vert.classic_menu">عرض الطلبات المقدمة</a>
+                    </li>
+                    <li>
+                        <a class="menu-item" href="{{ route('teachers.request_services.exam.create') }}" data-i18n="nav.templates.vert.classic_menu">طلب اختبار</a>
                     </li>
                 </ul>
             </li>
