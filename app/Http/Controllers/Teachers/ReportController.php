ede5455e40c47d0e9c511b2358af89e71ef34926
Hatim Hussein <hatem.fouad@furqangroup.com>
2021-11-10 23:28:55 +0200

    send monthly grade button (conditions (send or error)  can't send)
    conditions (send or error)  can't send

--- app/Http/Controllers/Teachers/ReportController.php
+++ app/Http/Controllers/Teachers/ReportController.php
@@ -18,6 +18,7 @@ use App\Teacher;
 use App\User;
 use App\Year;
 use Carbon\Carbon;
+use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -49,15 +50,15 @@ class ReportController extends Controller
         $students = User::query()->where('class_number', '=', $class_number)->orderBy('student_number', 'ASC')->get();
 
         $students_listener_names = User::query()
-                                        ->where('users.class_number', '=', $class_number)
-                                        ->select('name')
-                                        ->get()->pluck('name')->toArray();
+            ->where('users.class_number', '=', $class_number)
+            ->select('name')
+            ->get()->pluck('name')->toArray();
 
         $teachers_listener_names = Teacher::query()
-                                            ->join('classes_teachers', 'classes_teachers.teacher_email', '=', 'teachers.email')
-                                            ->where('classes_teachers.class_number', '=', $class_number)
-                                            ->select('teachers.name')
-                                            ->get()->pluck('name')->toArray();
+            ->join('classes_teachers', 'classes_teachers.teacher_email', '=', 'teachers.email')
+            ->where('classes_teachers.class_number', '=', $class_number)
+            ->select('teachers.name')
+            ->get()->pluck('name')->toArray();
 
         $listener_names = array_merge($students_listener_names, $teachers_listener_names);
 
@@ -231,12 +232,97 @@ class ReportController extends Controller
         return response()->json(['lesson_title' => $lesson->lesson_title], 201);
     }
 
+    public function getWorkingDaysCount($month, $year)
+    {
+
+        $currentMonth = Carbon::createFromDate($year, $month)->format('F');
+        $nextMonth = Carbon::createFromDate($year, $month)->addMonth();
+
+        $holidays[] = new \DatePeriod(
+            Carbon::parse("first saturday of $currentMonth $year"),
+            CarbonInterval::week(),
+            Carbon::parse("first day of " . $nextMonth->format('F') . " " . $nextMonth->year)
+        );
+
+        $holidays[] = new \DatePeriod(
+            Carbon::parse("first friday of $currentMonth $year"),
+            CarbonInterval::week(),
+            Carbon::parse("first friday of " . $nextMonth->format('F') . " " . $nextMonth->year)
+        );
+
+        $daysInMonth = Carbon::createFromDate($year, $month)->daysInMonth;
+        $holidaysCont = 0;
+
+        foreach ($holidays as $key => $days){
+            $holidaysCont += iterator_count($days);
+        }
+
+        return $daysInMonth - $holidaysCont;;
+    }
+
+    public function checkMonthlyReportInfo($user, $month_year)
+    {
+
+        $month = substr($month_year, -2);
+        $year  = substr($month_year, 0, 4);
+
+        if ($user->path == "قسم الهجاء"){
+            $pageNumber = $user->monthlyScores($month_year)->noorania_page_id;
+        }else{
+            $pageNumber = $user->monthlyScores($month_year)->lesson_page_id;
+        }
+
+        $reportsCount = Report::query()
+            ->whereMonth('created_at', '=', $month)
+            ->whereYear('created_at', '=', $year)
+            ->where('date', 'not like', '%Saturday%')
+            ->where('date', 'not like', '%Friday%')
+            ->where('student_id', '=', $user->id)
+            ->where(function ($query){
+                $query ->where('lesson_grade', '>=', 0);
+                $query ->orWhere('lesson_grade', '=', '-');
+            })->where(function ($query){
+                $query ->where('last_5_pages_grade', '>=', 0);
+                $query ->orWhere('last_5_pages_grade', '=', '-');
+            })->where(function ($query){
+                $query ->where('daily_revision_grade', '>=', 0);
+                $query ->orWhere('daily_revision_grade', '=', '-');
+            })->whereIn('absence', [0, -2, -5])
+            ->count();
+
+        $workingDays = $this->getWorkingDaysCount($month, $year);
+
+        if($reportsCount < $workingDays){
+            $workingDays = false;
+        }else{
+            $workingDays = true;
+        }
+
+        return ($pageNumber && $workingDays);
+    }
+
     public function sendReportTableMonthly(Request $request)
     {
         $user =  User::find($request->student_id);
 
-//      return view('emails.user.monthly_report', ['student_id' => $request->student_id]);
-        Notification::route('mail', [$user->father_mail, $user->mother_mail])->notify(new userReportMonthlyNotification($user));
+        if(!isset($request->date_filter) || is_null($request->date_filter)) {
+            $request['date_filter'] = date('Y') . '-' . date('m');
+        }
+
+        if (!$this->checkMonthlyReportInfo($user, $request->date_filter)){
+            session()->flash('error', 'يرجى التأكد من إدخال جميع بيانات الشهر ورقم الصفحة بشكل صحيح!');
+            return redirect()->route('teachers.report.table', $request->student_id);
+        }
+
+        try{
+            Notification::route('mail', [$user->father_mail, $user->mother_mail])->notify(new userReportMonthlyNotification($user));
+            $report = $user->monthlyScores($request->date_filter);
+            $report->update(['mail_status' => 1]);
+        }
+        catch(\Exception $e){
+            session()->flash('error', 'فشلت عملية ارسال التقرير الشهري!');
+            return redirect()->route('teachers.report.table', $request->student_id);
+        }
 
         session()->flash('success', 'تم ارسال التقرير الشهري بنجاح');
 
@@ -314,46 +400,46 @@ class ReportController extends Controller
         }
 
         $grades_report = Report::query()
-                        ->where('student_id', '=', $request->student_id)
-                        ->whereMonth('created_at', '=', $today->month)
-                        ->whereDay('created_at', '=', $today->day)
-                        ->whereYear('created_at', '=', $today->year)
-                        ->whereNotNull('lesson_grade')
-                        ->where('lesson_grade', '!=', '')
-                        ->whereNotNull('last_5_pages_grade')
+            ->where('student_id', '=', $request->student_id)
+            ->whereMonth('created_at', '=', $today->month)
+            ->whereDay('created_at', '=', $today->day)
+            ->whereYear('created_at', '=', $today->year)
+            ->whereNotNull('lesson_grade')
+            ->where('lesson_grade', '!=', '')
+            ->whereNotNull('last_5_pages_grade')
 //                        ->where('last_5_pages_grade', '!=', '')
-                        ->whereNotNull('behavior_grade')
+            ->whereNotNull('behavior_grade')
 //                        ->where('behavior_grade', '!=', '')
-                        ->whereNotNull('daily_revision_grade')
+            ->whereNotNull('daily_revision_grade')
 //                        ->where('daily_revision_grade', '!=', '')
-                        ->whereNotNull('listener_name')
+            ->whereNotNull('listener_name')
 //                        ->where('listener_name', '!=', '')
-                        ->whereNotNull('mistake')
-                        ->where('mistake', '!=', '')
-                        ->whereNotNull('alert')
-                        ->where('alert', '!=', '')
-                        ->first();
+            ->whereNotNull('mistake')
+            ->where('mistake', '!=', '')
+            ->whereNotNull('alert')
+            ->where('alert', '!=', '')
+            ->first();
 
         $lessons_report = Report::query()
-                    ->where('student_id', '=', $request->student_id)
-                    ->whereMonth('created_at', '=', $tomorrow->month)
-                    ->whereDay('created_at', '=', $tomorrow->day)
-                    ->whereYear('created_at', '=', $tomorrow->year)
-                    ->whereNotNull('new_lesson')
-                    ->where('new_lesson', '!=', '')
-                    ->whereNotNull('new_lesson_from')
-                    ->where('new_lesson_from', '!=', '')
-                    ->whereNotNull('new_lesson_to')
-                    ->where('new_lesson_to', '!=', '')
-                    ->whereNotNull('daily_revision')
-                    ->where('daily_revision', '!=', '')
-                    ->whereNotNull('daily_revision_from')
-                    ->where('daily_revision_from', '!=', '')
-                    ->whereNotNull('daily_revision_to')
-                    ->where('daily_revision_to', '!=', '')
-                    ->whereNotNull('number_pages')
-                    ->where('number_pages', '!=', '')
-                    ->first();
+            ->where('student_id', '=', $request->student_id)
+            ->whereMonth('created_at', '=', $tomorrow->month)
+            ->whereDay('created_at', '=', $tomorrow->day)
+            ->whereYear('created_at', '=', $tomorrow->year)
+            ->whereNotNull('new_lesson')
+            ->where('new_lesson', '!=', '')
+            ->whereNotNull('new_lesson_from')
+            ->where('new_lesson_from', '!=', '')
+            ->whereNotNull('new_lesson_to')
+            ->where('new_lesson_to', '!=', '')
+            ->whereNotNull('daily_revision')
+            ->where('daily_revision', '!=', '')
+            ->whereNotNull('daily_revision_from')
+            ->where('daily_revision_from', '!=', '')
+            ->whereNotNull('daily_revision_to')
+            ->where('daily_revision_to', '!=', '')
+            ->whereNotNull('number_pages')
+            ->where('number_pages', '!=', '')
+            ->first();
 
         if(is_null($grades_report) || is_null($lessons_report)){
             session()->flash('error', 'لم يتم ارسال التقرير اليومي يرجى التأكد من إدخال جميع البيانات!!');
@@ -539,11 +625,11 @@ class ReportController extends Controller
     {
         $ids= Report::where("mail_status","<>",1)->orderBy("id","desc")->limit(10)->get('class_number');
         $calses = ClassesTeachers::whereIn("class_number",$ids)->get();
-           foreach ($calses as $class) {
-                $classids=  Report::where("class_number",$class->class_number)->get('student_id');
-                $classids=  user::whereIn('student_number',$classids)->pluck('student_number')->toArray();
-                TeacherNotify::dispatch(["email" =>$class->teacher_email,"ids"=>$classids]);
-           }
+        foreach ($calses as $class) {
+            $classids=  Report::where("class_number",$class->class_number)->get('student_id');
+            $classids=  user::whereIn('student_number',$classids)->pluck('student_number')->toArray();
+            TeacherNotify::dispatch(["email" =>$class->teacher_email,"ids"=>$classids]);
+        }
     }
 
     public static function notifyTeacherFildReport($class)
--- database/seeds/ReportSeeder.php
+++ database/seeds/ReportSeeder.php
@@ -12,66 +12,46 @@ class ReportSeeder extends Seeder
      */
     public function run()
     {
-//        $today = Carbon::today();
-//        $today_created_at = Carbon::createFromDate($today->year, $today->month, $today->day)->format('Y-m-d H:i:s');
-//        $today_date = Carbon::createFromDate($today->year, $today->month, $today->day)->format('l d-m-Y');
-//
-//        $users = \App\User::all();
-//        foreach ($users as $user){
-//            $class = \App\Classes::where('class_number', '=', $user->class_number)->first();
-//            \App\Report::create([
-//                    'date' => $today_date,
-//                    'new_lesson' => 'test',
-//                    'student_id' => $user->id,
-//                    'class_id' => $class->id,
-//                    'created_at' => $today_created_at,
-//                ]
-//            );
-//        }
-
-
+        $month = 10;
         $students = \App\User::query()->get();
+
         foreach ($students as $student){
-            echo "1";
             $student->update([
                 "path" => trim($student->path),
             ]);
         }
 
-//        foreach ($students as $key => $student){
-//            $report = \App\Report::where('student_id', '=', $student->id)
-////                ->whereMonth('created_at', '=', 11)
-//                ->whereMonth('created_at', '=', 10)
-//                ->whereYear('created_at', '=', 2021)
-//                ->first();
-//
-//            if (!is_null($report)){
-//                echo $student->id . "\n";
-//
-//                DB::table('monthly_scores')->updateOrInsert(
-//                    [
-//                        'user_id' => $student->id,
-////                        'month_year' => "2021-11",
-//                        'month_year' => "2021-10",
-//                    ],
-//                    [
-//                        'new_lessons_not_listened' => getLessonsNotListenedCount($student->id, true),
-//                        'last_five_pages_not_listened' => getLastFivePagesNotListenedCount($student->id, true),
-//                        'daily_revision_not_listened' => getDailyRevisionNotListenedCount($student->id, true),
-//                        'absence_excuse_days' => getAbsenceCount($student->id, -2, true),
-//                        'absence_unexcused_days' => getAbsenceCount($student->id, -5, true),
-//                        'page_number' => 0,
-//                        'avg' => 100 + (
-//                                (getLessonsNotListenedCount($student->id, true) * -getPathDefaultGrade(getStudentPath($student->id), 'new_lesson')) +
-//                                (getLastFivePagesNotListenedCount($student->id, true) * -getPathDefaultGrade(getStudentPath($student->id), 'last_5_pages')) +
-//                                (getDailyRevisionNotListenedCount($student->id, true) * -getPathDefaultGrade(getStudentPath($student->id), 'daily_revision')) +
-//                                (getAbsenceCount($student->id, -2, true) * -2) +
-//                                (getAbsenceCount($student->id, -5, true) * -5)
-//                            ),
-//                    ]
-//                );
-//            }
-//        }
+        foreach ($students as $key => $student){
+            $report = \App\Report::where('student_id', '=', $student->id)
+                ->whereMonth('created_at', '=', $month)
+                ->whereYear('created_at', '=', 2021)
+                ->first();
+
+            if (!is_null($report)){
+                echo $student->id . "\n";
+
+                DB::table('monthly_scores')->updateOrInsert(
+                    [
+                        'user_id' => $student->id,
+                        'month_year' => "2021-" . $month,
+                    ],
+                    [
+                        'new_lessons_not_listened' => getLessonsNotListenedCount($student->id, $month),
+                        'last_five_pages_not_listened' => getLastFivePagesNotListenedCount($student->id, $month),
+                        'daily_revision_not_listened' => getDailyRevisionNotListenedCount($student->id, $month),
+                        'absence_excuse_days' => getAbsenceCount($student->id, -2, $month),
+                        'absence_unexcused_days' => getAbsenceCount($student->id, -5, $month),
+                        'avg' => 100 + (
+                                (getLessonsNotListenedCount($student->id, $month) * -getPathDefaultGrade(getStudentPath($student->id), 'new_lesson')) +
+                                (getLastFivePagesNotListenedCount($student->id, $month) * -getPathDefaultGrade(getStudentPath($student->id), 'last_5_pages')) +
+                                (getDailyRevisionNotListenedCount($student->id, $month) * -getPathDefaultGrade(getStudentPath($student->id), 'daily_revision')) +
+                                (getAbsenceCount($student->id, -2, $month) * -2) +
+                                (getAbsenceCount($student->id, -5, $month) * -5)
+                            ),
+                    ]
+                );
+            }
+        }
 
     }
 }
