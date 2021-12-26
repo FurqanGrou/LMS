83322bee2c2738e1b80651ae186ecbf563dbb79f
Hatim Hussein <91913484+HatemFurqan@users.noreply.github.com>
2021-12-27 00:38:12 +0200

    optimize performance time of code
    optimize performance time of code

--- database/seeds/ReportSeeder.php
+++ database/seeds/ReportSeeder.php
@@ -30,24 +30,33 @@ class ReportSeeder extends Seeder
             if (!is_null($report)){
                 echo $student->id . "\n";
 
+                $month_year = $report->created_at->format('Y-m');
+                $student_path = getStudentPath($report->student_id);
+
+                $new_lessons_not_listened = getLessonsNotListenedCount($student->id, $month);
+                $last_five_pages_not_listened = getLastFivePagesNotListenedCount($student->id, $month);
+                $daily_revision_not_listened = getDailyRevisionNotListenedCount($student->id, $month);
+                $absence_excuse_days = getAbsenceCount($student->id, -2, $month);
+                $absence_unexcused_days = getAbsenceCount($student->id, -5, $month);
+
                 DB::table('monthly_scores')->updateOrInsert(
                     [
                         'user_id' => $student->id,
                         'month_year' => "2021-" . $month,
                     ],
                     [
-                        'path' => getStudentPath($student->id),
-                        'new_lessons_not_listened' => getLessonsNotListenedCount($student->id, $month),
-                        'last_five_pages_not_listened' => getLastFivePagesNotListenedCount($student->id, $month),
-                        'daily_revision_not_listened' => getDailyRevisionNotListenedCount($student->id, $month),
-                        'absence_excuse_days' => getAbsenceCount($student->id, -2, $month),
-                        'absence_unexcused_days' => getAbsenceCount($student->id, -5, $month),
+                        'path' => $student_path,
+                        'new_lessons_not_listened' => $new_lessons_not_listened,
+                        'last_five_pages_not_listened' => $last_five_pages_not_listened,
+                        'daily_revision_not_listened' => $daily_revision_not_listened,
+                        'absence_excuse_days' => $absence_excuse_days,
+                        'absence_unexcused_days' => $absence_unexcused_days,
                         'avg' => 100 + (
-                                (getLessonsNotListenedCount($student->id, $month) * -getPathDefaultGrade(getStudentPath($student->id), 'new_lesson')) +
-                                (getLastFivePagesNotListenedCount($student->id, $month) * -getPathDefaultGrade(getStudentPath($student->id), 'last_5_pages')) +
-                                (getDailyRevisionNotListenedCount($student->id, $month) * -getPathDefaultGrade(getStudentPath($student->id), 'daily_revision')) +
-                                (getAbsenceCount($student->id, -2, $month) * -2) +
-                                (getAbsenceCount($student->id, -5, $month) * -5)
+                                ($new_lessons_not_listened * -getPathDefaultGrade($student_path, 'new_lesson')) +
+                                ($last_five_pages_not_listened * -getPathDefaultGrade($student_path, 'last_5_pages')) +
+                                ($daily_revision_not_listened * -getPathDefaultGrade($student_path, 'daily_revision')) +
+                                ($absence_excuse_days * -2) +
+                                ($absence_unexcused_days * -5)
                             ),
                     ]
                 );
