73b0da5900bbfdd73fd6710cf60729024538f562
Hatim Hussein <91913484+HatemFurqan@users.noreply.github.com>
2022-11-06 18:04:44 +0200

    log-history-searchable
    [NL-808]

--- app/Http/Controllers/Dashboard/AuditController.php
+++ app/Http/Controllers/Dashboard/AuditController.php
@@ -5,30 +5,60 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use App\Report;
 use App\Teacher;
+use App\User;
 use Illuminate\Http\Request;
 use OwenIt\Auditing\Models\Audit;
 
 class AuditController extends Controller
 {
-    public function index(Request $request)
+//    public function index(Request $request)
+//    {
+//
+//        $teachers = Teacher::query()->get();
+//        $students = User::query()->get();
+//
+//        if($request->has('teacher')){
+//            $audits = Audit::with('user')
+//                ->where('user_type', 'like','%Teacher%')
+//                ->where('user_id', $request['teacher'])
+//                ->where('event', '!=', 'created')
+//                ->orderBy('created_at', 'desc')
+//                ->paginate(25);
+//        }else{
+//            $audits = Audit::with('user')
+//                ->where('event', '!=', 'created')
+//                ->orderBy('created_at', 'desc')
+//                ->paginate(25);
+//        }
+//
+//        return view('admins.audits.index', ['audits' => $audits, 'teachers' => $teachers, 'students' => $students]);
+//    }
+
+    public function index()
     {
-
         $teachers = Teacher::query()->get();
+        $students = User::query()->get();
 
-        if($request->has('teacher')){
+        $student = null;
+        if (request()->student_id) {
+            $student = User::query()->findOrFail(request()->student_id);
+            $reports = Report::query()
+                ->where('student_id', '=', request()->student_id)
+                ->where('created_at', '>=', request()->date_from)
+                ->where('created_at', '<=', request()->date_to)
+                ->get();
             $audits = Audit::with('user')
-                ->where('user_type', 'like','%Teacher%')
-                ->where('user_id', $request['teacher'])
-                ->where('event', '!=', 'created')
-                ->orderBy('created_at', 'desc')
-                ->paginate(25);
+                ->whereIn('auditable_id', $reports->pluck('id')->toArray())
+                ->where('auditable_type', 'like', '%Report%')
+                ->orderBy('updated_at', 'desc')
+                ->paginate(30);
         }else{
             $audits = Audit::with('user')
                 ->where('event', '!=', 'created')
                 ->orderBy('created_at', 'desc')
-                ->paginate(25);
+                ->paginate(30);
         }
 
-        return view('admins.audits.index', ['audits' => $audits, 'teachers' => $teachers]);
+        return view('admins.audits.index', ['audits' => $audits, 'teachers' => $teachers, 'student' => $student, 'students' => $students]);
     }
 }
--- resources/views/admins/audits/index.blade.php
+++ resources/views/admins/audits/index.blade.php
@@ -2,87 +2,131 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h4 class="card-title">جميع التفاعلات</h4>
-                <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
-                <div class="heading-elements">
-                    <ul class="list-inline mb-0">
-                        <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
-                        <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
-                        <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
-                    </ul>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+
+                    @if(empty($student))
+                        <h4 class="card-title black font-weight-bold">
+                            جميع التعديلات على تقارير الطلاب
+                        </h4>
+                    @else
+                        <h4 class="card-title black font-weight-bold">
+                            جميع التعديلات على تقارير الطالب/ة <span class="badge badge-primary font-weight-bold">{{ $student->name }}</span>
+                        </h4>
+                    @endif
+
+                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                    <div class="heading-elements">
+                        <ul class="list-inline mb-0">
+                            <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <form action="{{ route('admins.audits.index') }}" method="GET" class="m-auto">
-                <select name="teacher" class="form-control">
-                    @foreach($teachers as $teacher)
-                        <option value="{{ $teacher->id }}">{{ $teacher->name }}</option>
-                    @endforeach
-                </select>
-                <button type="submit" class="btn btn-primary form-control white mt-1">بحث</button>
-            </form>
-
-            <div class="card-content collapse show">
-                <div class="card-body card-admins">
-                    <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
-                        <table id="tables" class="table table-striped table-bordered dataTable">
-                            <tbody>
-                                @foreach ($audits as $audit)
-                                    <tr>
-                                        <td>
-                                            <span>{{ $audit->created_at->timezone('asia/riyadh')->format('g:i A Y-m-d') }}</span>
-                                            <br>
-                                            <strong>
-                                                تم التعديل بواسطة:
-                                                {{ $audit->user->name }}
-                                            </strong>
-                                            <br>
-                                            <span>({{ substr($audit->auditable_type, 4) }})</span>
-                                            <br>
-                                            <strong>ID</strong>
-                                            <span>{{ $audit->auditable_id }}</span>
-                                        </td>
-                                    <td>
-                                        <strong>نوع العملية: {{ $audit->event }}</strong>
-                                        <br>
-                                        @foreach ($audit->getModified() as $attribute => $modified)
-                                            <ul>
-                                                <li>
-                                                    العمود: {{ $attribute }}
-                                                    <br>
-                                                    <strong> القيمة السابقة</strong>
-                                                    @if(isset($modified['old']))
-                                                        {{$modified['old']}}
-                                                    @endif
-                                                    <br>
-                                                    <strong>القيمة الجديدة</strong>
-                                                    @if (isset($modified['new']))
-                                                        {{$modified['new']}}
-                                                    @endif
-                                                </li>
-                                            </ul>
-                                        @endforeach
-                                    </td>
-                                    <td></td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-
-                        {{ $audits->links() }}
-                        <div class="box-body">
-
+                <form action="{{ route('admins.audits.index') }}" method="GET" class="m-auto">
+                    <div class="row">
+                        <div class="col-4">
+                            <div>
+                                <label for="teacher" class="font-weight-bold">من تاريخ:</label>
+                            </div>
+                            <input type="date" name="date_from" class="form-control black" value="{{ request()->date_from }}" required>
+                        </div>
+                        <div class="col-4">
+                            <div>
+                                <label for="teacher" class="font-weight-bold">إلى تاريخ:</label>
+                            </div>
+                            <input type="date" name="date_to" class="form-control black" value="{{ request()->date_to }}" required>
                         </div>
 
+                        <div class="col-4">
+                            <div>
+                                <label for="student_id" class="font-weight-bold">الطالب:</label>
+                            </div>
+                            <select name="student_id" id="student_id" class="form-control select2 black">
+                                @foreach($students as $student)
+                                    <option value="{{ $student->id }}" {{ request('student_id') == $student->id ? 'selected' : '' }}>{{ $student->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <dvi class="col-2">
+                            <button type="submit" class="btn btn-primary form-control white mt-1">بحث</button>
+                        </dvi>
+                    </div>
+                </form>
+
+                <div class="card-content collapse show">
+                    <div class="card-body card-admins">
+                        <div id="DataTables_Table_0_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4">
+                            <table id="tables" class="table table-bordered table-striped table-hover">
+                                <thead class="black">
+                                <tr>
+                                    <th>#</th>
+                                    <th>تاريخ التعديل</th>
+                                    <th>بواسطة</th>
+                                    <th>نوع العملية</th>
+                                    <th>البيانات المعدلة</th>
+                                </tr>
+                                </thead>
+                                <tbody class="font-weight-bold black">
+
+                                @forelse($audits as $audit)
+                                    <tr>
+                                        <td>
+                                            {{ $audit->auditable_id }}
+                                        </td>
+                                        <td>
+                                            {{ $audit->created_at->timezone('asia/riyadh')->format('A g:i - Y-m-d') }}
+                                        </td>
+                                        <td>
+                                            {{ @$audit->user->name }}
+                                        </td>
+                                        <td>
+                                            {{ $audit->event }}
+                                        </td>
+                                        <td style="width: 40%">
+                                            @foreach ($audit->getModified() as $attribute => $modified)
+                                                <ul>
+                                                    <li class="font-weight-bold"> مدخل: {{ $attribute }}</li>
+                                                    <li class="bg-danger white font-weight-bold w-50" style="padding: 5px; margin: 2px; border-radius: 5px;">
+                                                        القيمة السابقة:
+                                                        <br>
+                                                        @if(isset($modified['old']))
+                                                            {{$modified['old']}}
+                                                        @endif
+                                                    </li>
+                                                    <li class="bg-success white font-weight-bold w-50" style="padding: 5px; margin: 2px; border-radius: 5px; background-color: #05c581 !important">
+                                                        القيمة الجديدة:
+                                                        <br>
+                                                        @if(isset($modified['new']))
+                                                            {{$modified['new']}}
+                                                        @endif
+                                                    </li>
+                                                </ul>
+                                            @endforeach
+                                        </td>
+
+                                    </td>
+                                @empty
+                                    <h4 class="danger">لا يوجد نتائج بحث متوفرة</h4>
+                                @endforelse
+                                </tbody>
+                            </table>
+
+                            {{ $audits->withQueryString()->links() }}
+                            <div class="box-body">
+
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 
 @endsection
